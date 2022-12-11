d103f22efada44efa659ffd6123b4d4e8c7b3fb4
TMDT <tmdt.hk211@gmail.com>
2022-12-11 14:35:22 +0700

    [init] create game

---
+++ modules/game/logic.py
@@ -0,0 +1,69 @@
+import random
+
+
+class GameLogic:
+    BOARD_ROWS = 10
+    BOARD_COLS = 10
+
+    NUM_BOMBS = 20
+
+    BOMB_VALUE = -1
+
+    def __init__(self) -> None:
+        self.board = [[None for _ in range(GameLogic.BOARD_COLS)] for _ in range(GameLogic.BOARD_ROWS)]
+
+        self.dugCells: set[tuple(int, int)] = set()
+
+        self.plantBombs()
+        self.initValues()
+
+    def plantBombs(self) -> None:
+        availableCells: list[tuple[int, int]] = []
+        for i in range(GameLogic.BOARD_ROWS):
+            for j in range(GameLogic.BOARD_COLS):
+                availableCells.append((i, j))
+
+        for _ in range(GameLogic.NUM_BOMBS):
+            index = random.randint(0, len(availableCells) - 1)
+            row, col = availableCells[index]
+            del availableCells[index]
+            self.board[row][col] = GameLogic.BOMB_VALUE
+
+    def initValues(self) -> None:
+        for i in range(GameLogic.BOARD_ROWS):
+            for j in range(GameLogic.BOARD_COLS):
+                if self.board[i][j] == GameLogic.BOMB_VALUE: continue
+                self.board[i][j] = self.countBombsInNeighbor(i, j)
+
+    def countBombsInNeighbor(self, row: int, col: int) -> int: 
+        count = 0
+        for i in range(max(0, row - 1), min(len(self.board), row + 2)):
+            for j in range(max(0, col - 1), min(len(self.board[row]), col + 2)):
+                if i == row and j == col: continue
+                if self.board[i][j] == GameLogic.BOMB_VALUE: count += 1
+
+        return count
+
+    def getCell(self, row: int, col: int) -> int:
+        return self.board[row][col]
+
+    def dig(self, row: int, col: int, cells: list[tuple[int, int]]) -> bool:
+        if self.board[row][col] == GameLogic.BOMB_VALUE: return False
+
+        cells.append((row, col))
+        self.dugCells.add((row, col))
+        if self.board[row][col] != 0: return True
+        for i in range(max(0, row - 1), min(len(self.board), row + 2)):
+            for j in range(max(0, col - 1), min(len(self.board[row]), col + 2)):
+                if (i, j) in self.dugCells or self.board[i][j] == GameLogic.BOMB_VALUE: continue
+                self.dig(i, j, cells)
+
+        return True
+
+    def isEndGame(self) -> bool:
+        for i in range(GameLogic.BOARD_ROWS):
+            for j in range(GameLogic.BOARD_COLS):
+                if self.board[i][j] != GameLogic.BOMB_VALUE and (i, j) not in self.dugCells: return False
+        return True
+                
+                
---
+++ modules/game/scenes.py
@@ -0,0 +1,83 @@
+import pygame
+from components.button import Button
+from components.label import Label
+from components.scene import Scene
+from components.panel import Panel
+from components.shape import Line
+from modules.game.logic import GameLogic
+from utils.enum_types import MouseEvent, MouseEventContext
+from utils.json_reader import JsonReader
+import utils.constants as constants
+
+
+class GameScene(Scene):
+    CONFIG_FILE = "conf/game/GameScene.json"
+
+    FLAG = '#'
+
+    def __init__(self) -> None:
+        self.conf = JsonReader.load(GameScene.CONFIG_FILE)
+
+        self.logic = GameLogic()
+        self.isDigging = True
+        self.switchBtn = Button(conf=self.conf["switchBtn"])
+        self.boardPanel = Panel(conf=self.conf["boardPanel"])
+        self.width, self.height = self.boardPanel.getSize()
+        self.cellWidth, self.cellHeight = self.width // GameLogic.BOARD_COLS, self.height // GameLogic.BOARD_ROWS
+        self.addLines()
+        self.addValues()
+
+        self.boardPanel.addEventListener(MouseEvent.ON_TOUCH_END, self.onBoardClick)
+        self.switchBtn.addEventListener(MouseEvent.ON_TOUCH_END, self.onSwitchClick)
+
+    def addLines(self) -> None:
+        for i in range(GameLogic.BOARD_COLS + 1):
+            offset = 0 if i == 0 else -2
+            self.boardPanel.addChild(f"verticalLine{i}", Line({'startX': i * self.cellWidth + offset, 'endX': i * self.cellWidth + offset, 
+                'startY': 0, 'endY': self.height, "width": 2}))
+        for i in range(GameLogic.BOARD_ROWS + 1):
+            offset = 0 if i == 0 else -2
+            self.boardPanel.addChild(f"horizontalLine{i}", Line({'startX': 0, 'endX': self.width, 'startY': i * self.cellHeight + offset, 
+                'endY': i * self.cellHeight + offset, "width": 2}))
+    
+    def addValues(self) -> None:
+        for i in range(GameLogic.BOARD_ROWS):
+            for j in range(GameLogic.BOARD_COLS):
+                self.boardPanel.addChild(f"label_{i}_{j}", Label({'x': (j + 0.5) * self.cellWidth, 'y': (i + 0.5) * self.cellHeight, 
+                    'text': str(self.logic.getCell(i, j)), 'anchor': 'MID_CENTER', 'isVisible': False}))
+
+    def draw(self, screen: pygame.surface.Surface) -> None:
+        screen.fill(constants.BACKGROUND_COLOR)
+
+        self.boardPanel.draw(screen)
+        self.switchBtn.draw(screen)
+
+    def onBoardClick(self, context: MouseEventContext) -> None:
+        x, y = context['x'], context['y']
+        localX, localY = self.boardPanel.getLocalPosition(x, y)
+        row, col = localY // self.cellHeight, localX // self.cellWidth
+
+        if not self.isDigging:
+            cell: Label = self.boardPanel.getChild(f"label_{row}_{col}")
+            if cell.getText() == GameScene.FLAG:
+                cell.setText(str(self.logic.getCell(row, col)))
+                cell.setVisible(False)
+                return
+            cell.setText(GameScene.FLAG)
+            cell.setVisible(True)
+            return
+
+        cells: list[tuple[int, int]] = []
+        isSuccess = self.logic.dig(row, col, cells)
+        for row, col in cells:
+            self.boardPanel.getChild(f"label_{row}_{col}").setVisible(True)
+        if not isSuccess or self.logic.isEndGame():
+            self.endGame()
+            return
+
+    def onSwitchClick(self, _: MouseEventContext) -> None:
+        self.isDigging = not self.isDigging
+
+    def endGame(self) -> None:
+        print("END GAME, YOU SUCKS!!")
+        pass
